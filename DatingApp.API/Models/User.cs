cf2d348a9e8a8ce35fc95def6275b7761d013401
Igor Litvishchenko <igli@ciklum.com>
2019-09-09 16:28:10 +0300

    Implimitation of security

---
+++ DatingApp.API/Models/User.cs
@@ -0,0 +1,15 @@
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        internal string username;
+
+        public int Id {get; set;}
+        
+        public string Username {get; set;}
+
+        public byte[] PasswordHash {get; set;}
+
+        public byte[] PasswordSalt {get; set;}
+    }
+}
